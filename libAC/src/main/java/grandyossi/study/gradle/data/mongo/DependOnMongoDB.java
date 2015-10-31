74cdeb3a2ff69e2dc9656628d3465d406b21bf6d
@grandyossi <grandyossi@gmail.com>
2015-10-31 22:11:20 +0200

    base 2015-10-31 [ ] copy via 'common.gradle':     jar files from local cache into subproject's local dependency folder.

---
+++ libAC/src/main/java/grandyossi/study/gradle/data/mongo/DependOnMongoDB.java
@@ -0,0 +1,15 @@
+
+package grandyossi.study.gradle.data.mongo;
+import com.mongodb.async.client.MongoClient;
+
+
+/**
+ *
+ * @author (name="grandyossi" , date="2015-10-31") */
+public class DependOnMongoDB {
+
+    public DependOnMongoDB() {
+        final MongoClient  stam = null;
+    }
+    
+}
